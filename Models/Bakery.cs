c5f9e51d21f1512e31b657434d91593bb1d01a2d
Rachel Schieferstein <violenza@gmail.com>
2020-02-28 12:02:00 -0800

    creating functionality to select bread by type

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 
 namespace Bakery.Models 
 {
-  public class BreadBag 
+  public class Bread
   {
+    public string BName{ get; set; }
+    public string BNumber{ get; set; }
     public int BAmount{ get; set; }
     public int BPrice{ get; set; }
 
 
-    public Bread()
+    public Bread(string bName, string bNumber, int bAmount, int bPrice)
     {
-      BName = ""
-      BAmount = 0;
-      BPrice = 0;
+      BName = bName;
+      BNumber = bNumber;
+      BAmount = bAmount;
+      BPrice = bPrice;
     }
 
     public int BreadDiscounter()
@@ -25,25 +28,25 @@ namespace Bakery.Models
 
   }
 
-  public class PastryBag 
-  {
-    public int PAmount{ get; set; }
-    public int PPrice1{ get; set; }
-    public int PPrice2 {get; set; }
+  // public class Pastry 
+  // {
+  //   public int PAmount{ get; set; }
+  //   public int PPrice1{ get; set; }
+  //   public int PPrice2 {get; set; }
 
-    public Pastry()
-    {
-      PName = "";
-      PAmount = 0;
-      PPrice1 = 0;
-      PPrice2 = 0;
+  //   public Pastry(string pName)
+  //   {
+  //     PName = pName;
+  //     PAmount = 0;
+  //     PPrice1 = 0;
+  //     PPrice2 = 0;
 
-    }
+  //   }
 
-    public int PastryDiscounter()
-    {
-      // 3 for 5 (pp2) and 1 for 2 (pp1)
-      return (PAmount/3)*PPrice2 + (PAmount%3)*PPrice1;
-    }
-  }
+  //   public int PastryDiscounter()
+  //   {
+  //     // 3 for 5 (pp2) and 1 for 2 (pp1)
+  //     return (PAmount/3)*PPrice2 + (PAmount%3)*PPrice1;
+  //   }
+  // }
 }
--- Program.cs
+++ Program.cs
@@ -10,33 +10,62 @@ namespace Bakery
   {
     public static void Main()
     {
-      BreadBag newBread = new BreadBag();
-      PastryBag newPastry = new PastryBag();
+      Bread newBread = new Bread("", "", 0, 0);
+      // Pastry newPastry = new Pastry();
+
+      Bread french = new Bread("French Bread", "1", 0, 8);
+      Bread white = new Bread("White Bread", "2", 0, 5);
+      Bread rye = new Bread("Rye Bread", "3", 0, 6);
+      Bread pumpernickel = new Bread("Pumpernickel", "4", 0, 4);
+
+        
+      List<Bread> breadShelf = new List<Bread>(4) { french, white, rye, pumpernickel };
 
       Console.WriteLine("Welcome to Pierre's Bakery! We have a 'Buy 2 and get 1 free' sale on bread, 'discount on 3' sale on pastries. Press y to see our shelves.");
 
+      string response1 = Console.ReadLine();
+
       if (response1 == "y" || response1 == "Y")
       {
-      Console.WriteLine("Shelf list here");
+        foreach(Bread item in breadShelf)
+        {
+          Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+          Console.WriteLine(item.BNumber + "  " + item.BName + " --- $" + item.BPrice + " per loaf");
+        }
+
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Console.WriteLine("Which kind of bread would you like? Enter the number next to the name.");
+
+        string breadPick = Console.ReadLine();
+        
+        foreach(Bread item in breadShelf)
+          if(breadPick == item.BNumber)
+        {
+            newBread.BName = item.BName;
+            newBread.BNumber = item.BNumber;
+            newBread.BPrice = item.BPrice;
+        }
+
+         Console.WriteLine("How many loaves of bread would you like?");
+         string stringBread = Console.ReadLine();
+         newBread.BAmount = int.Parse(stringBread);
       }
-
-      Console.WriteLine("Which kind of bread would you like? Enter the number next to the name.");
-      breadCart.add()
-
-      Console.WriteLine("How many loaves of bread would you like?");
-      string stringBread = Console.ReadLine();
-      newBread.BAmount = int.Parse(stringBread);
+ 
+        //
+     
+      
 
       Console.WriteLine("Total Bread Price: $" + newBread.BreadDiscounter());
 
-      Console.WriteLine("How many pastries would you like?");
-      string stringPastry = Console.ReadLine();
-      newPastry.PAmount = int.Parse(stringPastry);
+      // Console.WriteLine("How many pastries would you like?");
+      // string stringPastry = Console.ReadLine();
+      // newPastry.PAmount = int.Parse(stringPastry);
 
-      Console.WriteLine("Total Pastry Price: $" + newPastry.PastryDiscounter());
+      // Console.WriteLine("Total Pastry Price: $" + newPastry.PastryDiscounter());
 
-      Console.WriteLine("Your total order price comes to $" + (newBread.BreadDiscounter()+newPastry.PastryDiscounter())+".");
+      // Console.WriteLine("Your total order price comes to $" + (newBread.BreadDiscounter()+newPastry.PastryDiscounter())+".");
 
+      }
     }
   }
-}
